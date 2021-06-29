0db673c6027596fbd205e6f3aa3c92ef4c08df53
Prince Kafui <amexoprince@gmail.com>
2021-06-28 19:38:02 -0900

    reactpizzamania

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,43 +1,29 @@
 import React from "react"
 
 function Navbar() {
-    return (
-
-        <
-        nav className = "navbar navbar-default navbar-me" >
-        <
-        div className = "container-fluid" >
-        <
-        div className = "navbar-header" >
+    return ( 
+        
+        <nav className = "navbar navbar-default navbar-me">
+        <div className = "container-fluid" >
+        <div className = "navbar-header" >
 
 
-        <
-        a className = "navbar-brand"
-        href = "#" > PizzaMania < /a>  <
-        /div>
+        <a className = "navbar-brand" href = "#" > PizzaMania </a> 
+        </div>
 
-        <
-        div className = "collapse navbar-collapse"
-        id = "bs-example-navbar-collapse-1" >
+        <div className = "collapse navbar-collapse" id = "bs-example-navbar-collapse-1" >
 
-        <
-        ul className = "nav navbar-nav navbar-right" >
-        <
-        li > < a href = "#" > Home < /a></li >
-        <
-        li > < a href = "#" > About Pizza < /a></li >
-        <
-        li > < a href = "#" > Pizza Types < /a></li >
+        <ul className = "nav navbar-nav navbar-right" >
+        <li> <a href = "#"> Home </a></li>
+        <li> <a href = "#"> About Pizza </a></li >
+        < li > < a href = "#"> Pizza Types </a></li >
 
-        <
-        /ul> <
-        /div>
+        </ul>
+        </div>
 
-        <
-        /div>
+        </div>
 
-        <
-        /nav>
+        </nav>
 
 
     );
