307c190e3bc68f5bcd096cef3525e0b857dac6fa
Prince Kafui <amexoprince@gmail.com>
2021-06-28 20:00:22 -0900

    reactpizzamania

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-
 const Footer = () => {
-    return (
-
-        <
+    return ( <
         div class = "footer" >
         <
         div class = "container" >
         <
         div class = "row" >
         <
-        p class = "text-center" > CopyRight 2021 PizzaMania All Rights Reserved. < /p>  <
-        /div>  <
-        /div>  <
+        p class = "text-center" >
+        CopyRight 2021 PizzaMania All Rights Reserved. <
+        /p> <
+        /div> <
+        /div> <
         /div>
-
     );
-}
+};
 
 export default Footer;
